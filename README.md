5273726d10a4ca31bce3bffc032c6db4514e8025
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-07 15:00:46 +0200

    Calling the search function and using a "search state" state cell

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 4. Added a NavBar component
 5. Using NavLinks and `activeStyle` to highlight the current apge in the nav bar
 6. Added a controlled component to DiscoverMoviesPage
+7. Calling the search function and using a "search state" state cell
--- src/pages/DiscoverMoviesPage.tsx
+++ src/pages/DiscoverMoviesPage.tsx
@@ -1,11 +1,32 @@
 // src/pages/DiscoverMoviesPage.tsx
 import React, { useState } from "react";
 
+type SearchState =
+  | { status: "idle" }
+  | { status: "loading" }
+  | { status: "success"; data: any } // todo: specify the data type too
+  | { status: "error"; error: any };
+
 export default function DiscoverMoviesPage() {
   const [searchText, setSearchText] = useState("");
+  const [state, setState] = useState<SearchState>({ status: "idle" });
 
-  const search = () => {
-    console.log("TODO search movies for:", searchText);
+  const search = async () => {
+    console.log("Start searching for:", searchText);
+    setState({ status: "loading" });
+
+    // Best practice: encode the string so that special characters
+    //  like '&' and '?' don't accidentally mess up the URL
+    const queryParam = encodeURIComponent(searchText);
+  
+    // Option A: use the browser-native fetch function
+    const data = await fetch(
+      `https://omdbapi.com/?apikey=b3d9013d&s=${queryParam}`
+    ).then((r) => r.json());
+
+    setState({ status: "success", data });
+
+    console.log("Success!", data);
   };
 
   return (
