a6c2cd25bf6276331938d76a521623d15e68cd39
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-07 14:47:11 +0200

    Added a NavBar component

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. Created new app with `create-react-app`
 2. Set up `react-router-dom`
 3. Added three top-level page components with `<Route>`s within a `<Switch>`
+4. Added a NavBar component
--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 
+import NavBar from './components/NavBar';
+
 export default function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route path="/discover" component={DiscoverMoviesPage} />
         <Route path="/about" component={AboutPage} />
