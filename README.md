88d1722fac1918b213b66670043899c51b6c7196
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-07 14:45:00 +0200

    Added three top-level page components

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 1. Created new app with `create-react-app`
 2. Set up `react-router-dom`
+3. Added three top-level page components with `<Route>`s within a `<Switch>`
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
 
-function App() {
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
-
-export default App;
