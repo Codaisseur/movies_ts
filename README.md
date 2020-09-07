42377adc4e0031b66206739cec33aad246cb9b4e
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-07 14:51:00 +0200

    Added a controlled component to DiscoverMoviesPage

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 3. Added three top-level page components with `<Route>`s within a `<Switch>`
 4. Added a NavBar component
 5. Using NavLinks and `activeStyle` to highlight the current apge in the nav bar
+6. Added a controlled component to DiscoverMoviesPage
--- src/pages/DiscoverMoviesPage.tsx
+++ src/pages/DiscoverMoviesPage.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+// src/pages/DiscoverMoviesPage.tsx
+import React, { useState } from "react";
 
 export default function DiscoverMoviesPage() {
+  const [searchText, setSearchText] = useState("");
+
+  const search = () => {
+    console.log("TODO search movies for:", searchText);
+  };
+
   return (
     <div>
-      <h1>Discover</h1>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
     </div>
   );
 }
